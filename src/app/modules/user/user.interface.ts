12c0b3156e3ae0d00534fbe5dcebcd5d0153450e
fai-sas <craftedbyfaisal@gmail.com>
2024-07-08 18:48:35 +0600

    user model added

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,3 +1,7 @@
+/* eslint-disable no-unused-vars */
+import { Model } from 'mongoose'
+import { USER_ROLE } from './user.constant'
+
 export interface TUser {
   name: string
   email: string
@@ -5,3 +9,16 @@ export interface TUser {
   role: 'user' | 'admin'
   address?: string
 }
+
+export interface UserModel extends Model<TUser> {
+  //instance methods for checking if the user exist
+  isUserExists(id: string): Promise<TUser>
+
+  //instance methods for checking if passwords are matched
+  isPasswordMatched(
+    plainPassword: string,
+    hashedPassword: string
+  ): Promise<boolean>
+}
+
+export type TUserRole = keyof typeof USER_ROLE
